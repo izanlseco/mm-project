704c5072c7ffb35a6ffe0cec50426e64b9cc9bba
Izan Lopez Seco <IzanLS09@gmail.com>
2021-09-18 14:04:58 +0200

    fontawesome, restyling, emojis

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import * as React from "react";
 import "../assets/custom.scss";
 import JSONData from "../assets/data.json";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../assets/logo_black.png";
 const pageStyles = {
   color: "#232129",
   padding: 96,
@@ -20,39 +22,75 @@ const cardStyle = {
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Minecraft mod manager</title>
-      <h1 class="title">Minecraft mods</h1>
-      <div class="columns is-multiline">
-        {JSONData.map((data, index) => (
-          <div class="column is-one-quarter">
-            <div class="card" style={cardStyle}>
-              <div class="card-content">
-                <div class="content">
-                  <a
-                    style={linkStyle}
-                    href={`${data.url}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {data.name}
-                  </a>
-                  <p class="subtitle">v{data.version}</p>
-                </div>
-              </div>
-              <footer class="card-footer">
-                <a href="#" class="card-footer-item">
-                  Edit
-                </a>
-                <a href="#" class="card-footer-item">
-                  delete
-                </a>
-              </footer>
-            </div>
+    <>
+      <nav
+        className="navbar is-light"
+        role="navigation"
+        aria-label="main-navigation"
+      >
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img src={Logo} alt="logo" />
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item is-size-4" href="/home">
+              Home
+            </a>
           </div>
-        ))}
-      </div>
-    </main>
+        </div>
+      </nav>
+      <main style={pageStyles}>
+        <h1 className="title is-uppercase is-family-code">Minecraft mods</h1>
+        <div className="columns is-multiline">
+          {JSONData.map((data, index) => (
+            <div key={index} className="column is-one-quarter">
+              <div className="card" style={cardStyle}>
+                <div className="card-content">
+                  <div className="content">
+                    <a
+                      className="has-text-link is-capitalized is-size-5"
+                      style={linkStyle}
+                      href={`${data.url}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {data.name}
+                    </a>
+                    <p className="subtitle">v{data.version}</p>
+                  </div>
+                </div>
+                <footer className="card-footer">
+                  <a href="/edit" className="card-footer-item is-size-5">
+                    Edit
+                  </a>
+                  <a href="/delete" className="card-footer-item is-size-5">
+                    Delete
+                  </a>
+                </footer>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <a class="button is-text" href="/">
+            <FontAwesomeIcon icon={faCaretUp} size="3x" />
+          </a>
+          <br />
+          Made with ❤️ by{" "}
+          <a href="https://www.izanlseco.com" target="_blank" rel="noreferrer">
+            Izan López
+          </a>
+          <span> & </span>
+          <a href="https://pattyogaliano.com/" target="_blank" rel="noreferrer">
+            Patty O'Galiano
+          </a>
+        </div>
+      </footer>
+    </>
   );
 };
 
